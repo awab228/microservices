af609ba8e495c7c5fcbf46f8fec039efb2724958
Awab Omer <awab228@yahoo.com>
2022-12-19 23:50:34 +0200

    Adds a Log for easy Observation

--- fraud/src/main/java/com/sgx/fraud/FraudController.java
+++ fraud/src/main/java/com/sgx/fraud/FraudController.java
@@ -1,17 +1,20 @@
 package com.sgx.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping (path = "{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
         Boolean isFraudulentCustomer = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud Check Request for customer {}", customerId);
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
