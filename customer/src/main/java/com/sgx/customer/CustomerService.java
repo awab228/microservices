0f058df89a89f187fe9582596f2f55841dcf90b0
Awab Omer <awab228@yahoo.com>
2022-12-20 02:27:06 +0200

    Changes The Rest Template to use Auto Service Discovery, and also LoadBalance the Requests

--- customer/src/main/java/com/sgx/customer/CustomerService.java
+++ customer/src/main/java/com/sgx/customer/CustomerService.java
@@ -17,7 +17,7 @@ public class CustomerService {
                 .build();
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://localhost:8082/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                     FraudCheckResponse.class,
                     customer.getId()
         );
