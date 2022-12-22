b8a1181035641cc2120850017a63d94eb33f075e
Awab Omer <awab228@yahoo.com>
2022-12-23 01:57:03 +0200

    Adds Simple LoadBalancer Using Spring API GateWay and Configure it to handle customers endpoint.

---
+++ apigw/src/main/java/com/sgx/apigw/ApiGwApplication.java
@@ -0,0 +1,10 @@
+package com.sgx.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ApiGwApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwApplication.class,args);
+    }
+}
