13463b2771263c0dd460579f24dc0390d7541927
Awab Omer <awab228@yahoo.com>
2022-12-23 20:08:14 +0200

    Adds RabbitMQ Notification Queue And its Consumer.

--- notification/src/main/java/com/sgx/notifications/NotificationsApplication.java
+++ notification/src/main/java/com/sgx/notifications/NotificationsApplication.java
@@ -3,7 +3,11 @@ package com.sgx.notifications;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.sgx.notifications",
+                "com.sgx.ampq"
+})
 public class NotificationsApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class,args);
