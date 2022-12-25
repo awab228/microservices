0925eb46eb1b24fc5b3c7bb4d9b1d8c29e1b9dc1
Awab Omer <awab228@yahoo.com>
2022-12-26 00:37:24 +0200

    Refactors the OpenFeign Clients to use Kubernetes when deployed to k8s.

--- customer/src/main/java/com/sgx/customer/CustomerApplication.java
+++ customer/src/main/java/com/sgx/customer/CustomerApplication.java
@@ -3,6 +3,8 @@ package com.sgx.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages ={
@@ -11,6 +13,9 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
         }
 )
 @EnableFeignClients(basePackages = "com.sgx.clients")
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class,args);
--- notification/src/main/java/com/sgx/notifications/NotificationsApplication.java
+++ notification/src/main/java/com/sgx/notifications/NotificationsApplication.java
@@ -2,11 +2,16 @@ package com.sgx.notifications;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
                 "com.sgx.notifications",
                 "com.sgx.ampq"
+})
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
 })
 public class NotificationsApplication {
     public static void main(String[] args) {
